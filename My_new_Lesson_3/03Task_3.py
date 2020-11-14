06ca388152d12d79224a881b01fc257f4b9f1437
AndreyPasternak <73710835+AndreyPasternak@users.noreply.github.com>
2020-11-14 13:37:10 +0300

    Delete 03Task_3.py

--- My_new_Lesson_3/03Task_3.py
+++
@@ -1,8 +0,0 @@
-def my_func(ar1, ar2, ar3):
-    res = sum([ar1, ar2, ar3]) - min([ar1, ar2, ar3])
-    return res
-ar1 = float(input('Ввведите первое число:   '))
-ar2 = float(input('Ввведите второе число:   '))
-ar3 = float(input('Ввведите второе число:   '))
-value = my_func(ar1, ar2, ar3)
-print(value)
